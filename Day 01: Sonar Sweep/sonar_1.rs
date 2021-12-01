7dac5c62a0d97867aa8be1d8ef263ba3eb2a925b
Robbert Gurdeep Singh <git@beardhatcode.be>
2021-12-01 23:35:51 +0100

    Day one maybe cleaner

--- Day 01: Sonar Sweep/sonar_1.rs
+++ Day 01: Sonar Sweep/sonar_1.rs
@@ -3,8 +3,13 @@ use std::io::{stdin, BufRead};
 fn main() {
     let s = stdin();
     let lines = s.lock().lines();
-    let mut b = lines.filter_map(|l| l.ok().and_then(|v| (v.parse::<u32>()).ok()));
-    let first = b.next().unwrap();
+    let mut b = lines.map(|l| {
+        l.ok()
+            .and_then(|v| (v.parse::<u32>().ok()))
+            .expect("Could not line")
+    });
+    let first = b.next().expect("Need at least one value");
+
     let (_, inc) = b.fold((first, 0), |(prev, inc), x| (x, ((x > prev) as u32) + inc));
     println!("{:?}", inc);
 }
